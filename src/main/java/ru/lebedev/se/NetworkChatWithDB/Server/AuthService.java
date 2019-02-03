ca6325a962c0f9f35cca10ff5eaea864ea019951
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-02-03 18:49:31 +0300

    Added chat from previous level

---
+++ src/main/java/ru/lebedev/se/NetworkChatWithDB/Server/AuthService.java
@@ -0,0 +1,107 @@
+package NetworkChatWithDB.Server;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class AuthService {
+
+    private static Connection connection;
+    private static Statement stmt;
+
+    public static void connect() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:mainDB.db");
+            stmt = connection.createStatement();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static String getNickByLoginAndPass(String login, String pass) {
+        String sql = String.format("SELECT nickname, password FROM main\n" +
+                "WHERE login = '%s'", login);
+
+        int myhash = pass.hashCode();
+
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                String nick = rs.getString(1);
+                int dbHash = rs.getInt(2);
+
+                if (myhash == dbHash) {
+                    return nick;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static synchronized void addUser(String login, String pass, String nick) {
+        String sql = String.format("INSERT INTO main (login, password, nickname)" +
+                "VALUES ('%s', '%s', '%s')", login, pass.hashCode(), nick);
+        try {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    blacklist
+    CREATE TABLE blacklist (
+    id    INTEGER PRIMARY KEY
+                  NOT NULL
+                  UNIQUE,
+    user  TEXT    NOT NULL,
+    black TEXT    NOT NULL
+);
+     */
+
+    public static synchronized ArrayList<String> getBlackList(String user) {
+        ArrayList<String> list = new ArrayList<String>();
+        ResultSet rs = null;
+        try {
+            String sql = String.format("SELECT black FROM blacklist\n" +
+                    "WHERE user = '%s'", user);
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                String black = rs.getString("black");
+                list.add(black);
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        list.clear();
+        return list;
+    }
+
+    public static synchronized void addToBlackList(String user, String black) {
+        String sql = String.format("INSERT INTO blacklist (user, black)" +
+                "VALUES ('%s', '%s')", user, black);
+        try {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/lebedev/se/NetworkChatWithDB/Server/ClientHandler.java
@@ -0,0 +1,163 @@
+package NetworkChatWithDB.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler {
+
+    Socket socket = null;
+    DataInputStream in;
+    DataOutputStream out;
+    Server server;
+    private int counterActivity; //счетчик активности
+    ArrayList<String> blackList;
+
+    public String getNick() {
+        return nick;
+    }
+
+    String nick;
+    boolean blackInList; // наличие в черном списке
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.counterActivity = 0;
+            this.blackList = new ArrayList<>();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/auth")) {
+                                String[] tokens = str.split(" ");
+                                String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                                if (newNick != null) {
+                                    if (!server.isNickBusy(newNick)) {
+                                        sendMsg("/authok");
+                                        nick = newNick;
+                                        server.subscribe(ClientHandler.this);
+                                        blackList = AuthService.getBlackList(nick);
+                                        break;
+                                    } else {
+                                        sendMsg("Учетная запись уже используется!");
+                                    }
+                                } else {
+                                    sendMsg("Неверный логин/пароль!");
+                                }
+                            }
+                        }
+
+                        /* Запуск счетчика активности */
+                        counterActivity();
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/")) {
+                                if (str.equals("/end")) {
+                                    out.writeUTF("/serverClosed");
+                                }
+                                if (str.equals("/bl")) {
+                                    sendMsg("В вашем черном списке:");
+                                    for (String s : blackList) {
+                                        sendMsg(s);
+                                    }
+                                    sendMsg("-------------------");
+                                }
+                                if (str.startsWith("/w ")) {
+                                    counterActivity = 0;
+                                    String[] tokens = str.split(" ", 3);
+                                    server.sendPersonalMsg(ClientHandler.this, tokens[1], tokens[2]);
+                                }
+                                if (str.startsWith("/blacklist ")) {
+                                    counterActivity = 0;
+                                    String[] tokens = str.split(" ");
+                                    String blackCandidate = tokens[1];
+                                    for (int i = 0; i < blackList.size(); i++) { // проверяем наличие в списке
+                                        if (blackList.get(i).equals(blackCandidate)) {
+                                            blackInList = true;
+                                            break;
+                                        } else blackInList = false;
+                                    }
+                                    if (!blackInList) {
+                                        blackList.add(blackCandidate); // добавляем в локальный список
+                                        AuthService.addToBlackList(nick, blackCandidate); // добавляем в бд
+                                        sendMsg("Вы добавили пользователя " + blackCandidate + " в черный список");
+                                    } else sendMsg("" + blackCandidate + " уже есть в черном списке");
+                                }
+                            } else {
+                                counterActivity = 0;
+                                server.broadcastMsg(ClientHandler.this, nick + ": " + str);
+                            }
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        server.unsubscribe(ClientHandler.this);
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkBlackList(String nick) {
+        return blackList.contains(nick);
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void counterActivity() {
+        Thread activity = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(10000); // 1 минута = 60000 мс - 10 секунд
+                    ++counterActivity;
+                    if (counterActivity > 12) { // 6 - 1 мин, если больше 2 мин, то выкидываем из чата
+                        sendMsg("Отключение из-за неактивности в чате");
+                        Thread.sleep(1000);
+                        out.writeUTF("/serverClosed");
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        activity.start();
+    }
+}
