777035d0f599770064b984b0ea4a849b7ef12d77
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-02-11 01:34:01 +0300

    HomeWork4

---
+++ src/main/java/ru/lebedev/se/HomeWork4/ReadFileWriteToCon.java
@@ -0,0 +1,12 @@
+package ru.lebedev.se.HomeWork4;
+
+/**
+ * Java 3, JavaLevel3.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 11.02.2019
+ * @link https://github.com/Centnerman
+ */
+
+public class ReadFileWriteToCon {
+}
