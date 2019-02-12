84274fd529a24c4aa41124a9d891ab482afe8553
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-02-13 01:43:06 +0300

    Homework4

--- src/main/java/ru/lebedev/se/HomeWork4/PageReader.java
+++ src/main/java/ru/lebedev/se/HomeWork4/PageReader.java
@@ -1,5 +1,9 @@
 package ru.lebedev.se.HomeWork4;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
 /**
  * Java 3, JavaLevel3.
  *
@@ -8,5 +12,52 @@ package ru.lebedev.se.HomeWork4;
  * @link https://github.com/Centnerman
  */
 
+/*
+4. Написать консольное приложение, которое умеет постранично читать текстовые файлы
+(размером > 10 mb). Вводим страницу (за страницу можно принять 1800 символов), программа
+выводит ее в консоль. Контролируем время выполнения: программа не должна загружаться
+дольше 10 секунд, а чтение – занимать свыше 5 секунд.
+ */
+
 public class PageReader {
+
+    public static final String FILE_NAME = "bigtext.txt";
+
+    private static final int PAGE_SIZE = 1800;
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите номер страницы: ");
+            int pageNumber = scanner.nextInt();
+            if (pageNumber == -1) {
+                scanner.close();
+                break;
+            }
+
+            long startTime = System.currentTimeMillis(); // засекаем время
+
+            try (RandomAccessFile raf = new RandomAccessFile(FILE_NAME, "r")) {
+                int startPosition = (pageNumber - 1) * PAGE_SIZE;
+                raf.seek(startPosition);
+                int counter = 0;
+
+                while (raf.getFilePointer() != (startPosition + PAGE_SIZE)) {
+                    String line = String.valueOf((char) raf.read()); // Выводим с перекодировкой
+                    String decodeLine = new String(line.getBytes("ISO-8859-1"), "windows-1251");
+                    System.out.print(decodeLine);
+                    raf.seek(startPosition + (++counter));
+                }
+                System.out.println();
+                System.out.println("Время на поиск и чтение страницы: " + (System.currentTimeMillis() - startTime) + "мс");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/ru/lebedev/se/HomeWork4/ReadFileWriteToCon.java
+++ src/main/java/ru/lebedev/se/HomeWork4/ReadFileWriteToCon.java
@@ -1,5 +1,9 @@
 package ru.lebedev.se.HomeWork4;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Java 3, JavaLevel3.
  *
@@ -8,5 +12,73 @@ package ru.lebedev.se.HomeWork4;
  * @link https://github.com/Centnerman
  */
 
+/*
+2. Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
+ */
+
 public class ReadFileWriteToCon {
+
+    /**
+     * Name reading file.
+     */
+    private static final String FILE_NAME = "texttoread.txt";
+
+    /**
+     * Array bytes from file.
+     */
+    public static byte[] bytes;
+
+    /**
+     * Simple text for writing into file.
+     */
+    public static final String TEXT = "In vino veritas!"; // строка для записи
+
+
+    public static void main(String[] args) {
+
+        // Запись
+        try (FileOutputStream fos = new FileOutputStream(FILE_NAME)) {
+
+            // перевод строки в байты
+            byte[] buffer = TEXT.getBytes();
+            fos.write(buffer, 0, buffer.length);
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("Файл " + FILE_NAME + " записан");
+
+        // Чтение
+        try (FileInputStream fin = new FileInputStream(FILE_NAME)) {
+
+            System.out.printf("Файл весит: %d байт \n", fin.available());
+            bytes = new byte[fin.available()]; // массив длиной в файл
+
+            int readBite = -1;
+            int countArr = 0; // переходим на начало массива
+
+            // Читаем до конца файла
+            while ((readBite = fin.read()) != -1) {
+                bytes[countArr++] = (byte) readBite;
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // Выводим в виде символов
+        for (int i = 0; i < bytes.length; i++) {
+
+            System.out.print((char) bytes[i]);
+        }
+        System.out.println("");
+
+        // Выводим в цифровом виде до пустой ячейки
+        for (int i = 0; i < bytes.length; i++) {
+
+            if (bytes[i] != 0) System.out.print(bytes[i] + " ");
+            else return;
+        }
+    }
 }
