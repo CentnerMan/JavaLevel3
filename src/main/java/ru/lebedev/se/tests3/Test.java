cdba0b958c04097b7caa34de0482d3e423a4873a
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-02-25 19:45:58 +0300

    test

---
+++ src/main/java/ru/lebedev/se/tests3/Test.java
@@ -0,0 +1,18 @@
+package ru.lebedev.se.tests3;
+
+/**
+ * Java 3, JavaLevel3.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 24.02.2019
+ * @link https://github.com/Centnerman
+ */
+
+public abstract class Test {
+    public static void main(String[] args) {
+//        File myPath = new File("k://java/a/b");
+//        myPath.mkdir();
+        System.out.println(int.class);
+          ;
+    }
+}
